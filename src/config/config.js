10abf51816680fd0fdae42401a312f73bd3c9a29
Patryk Kaczmarek <kaczmarek.patryk94@gmail.com>
2022-02-17 00:09:01 +0100

    added some commands

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,9 @@
-const dotenv = require("dotenv").config({ path: __dirname + "/./../../.env" })
+const dotenv = require("dotenv").config({ path: __dirname + "/./../../.env" });
 
 module.exports = {
-    TOKEN: process.env.token,
-    PREFIX: process.env.prefix,
-    OWNER: process.env.owner,
-  }
+  TOKEN: process.env.token,
+  CLIENT_ID: process.env.clientId,
+  GUILD_ID: process.env.guildId,
+  PREFIX: process.env.prefix,
+  OWNER: process.env.owner,
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const { TOKEN } = require("./config/config.js")
+const { TOKEN, CLIENT_ID, GUILD_ID } = require("./config/config.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const commands = [
+  new SlashCommandBuilder()
+    .setName("clear100")
+    .setDescription("Usuwa 100 wiadomości na kanale.")
+    .setDefaultPermission(false),
+  new SlashCommandBuilder()
+    .setName("info")
+    .setDescription("Zwraca informajce o bocie.!"),
+].map((command) => command.toJSON());
+
+const rest = new REST({ version: "9" }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("ready", () => {
+  console.log(`Zalogowano jako ${client.user.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+  const { commandName } = interaction;
+
+  if (commandName === "clear100") {
+    await interaction.channel.bulkDelete(100);
   }
+
+  if (commandName === "info") {
+    const embed = new MessageEmbed()
+      .setTitle("Snorlaks Bot")
+      .setColor(0x00a2ff)
+      .setDescription("Oryginalny bot Snorlaks, nie żaden podrabianiec.")
+      .setFooter({ text: "Bot w fazie testów" })
+      .addField("Autor", "patrol", true)
+      .addField("Wersja", "1.0", true);
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+    interaction.delete;
+  }
+});
+
+client.on("messageCreate", (msg) => {
+  // console.log(msg.content);
+  // if (msg.content === "!clear") {
+  //   msg.channel.bulkDelete(100);
+  // }
 });
 
 client.login(TOKEN);
